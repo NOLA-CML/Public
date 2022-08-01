eb83a1df169deac0c452eb906fa881db3b8f0eca
Cory <NOLA-CML@users.noreply.github.com>
2022-08-01 09:56:10 -0500

    Update NinjaPrinterInstall.ps1
    Final production version.

--- NinjaPrinterInstall.ps1
+++ NinjaPrinterInstall.ps1
@@ -1,3 +1,6 @@
+$tempFolder = "$env:TEMP\printer"
+$log = "$tempFolder\printer.log"
+Start-Transcript -Path $log
 #Modification of the script from Graham Pocta posted here: https://ninjarmm.zendesk.com/hc/en-us/community/posts/7102933603213--Full-printer-management-script
 
 ##############################################################################################
@@ -7,15 +10,10 @@
 #                                                                                            #
 #  Requirements:                                                                             #
 #   -Documentation template set up for Printers and Printers To Delete                       #
-#   -Tested on Windows 10/11 x64                                                             #
+#   -Tested on Windows 10 x64                                            					 #
 #   -If installing multiple printers using the same driver, make sure they use the same      #
 #    version number                                                                          #
 ##############################################################################################
-
-$webClient = New-Object net.webclient
-
-#Gets host's current gateway addresses
-$Gateways = Get-NetIPConfiguration | Foreach IPv4DefaultGateway | Select -ExpandProperty NextHop
 
 #Returns list of printers in org
 $Printers = Ninja-Property-Docs-Names "Printers"
@@ -34,31 +32,22 @@ foreach ($Printer in $Printers){
 	Write-Host "Name: $printerName"
 	$printerIP = Ninja-Property-Docs-Get "Printers" "$printerName" "ipAddress"
 	Write-Host "IP: $printerIP"
-	$driveFile = Ninja-Property-Docs-Get "Printers" "$printerName" "driverFile"
+	$driverFile = Ninja-Property-Docs-Get "Printers" "$printerName" "driverFilename"
 	Write-Host "Driver Filename: $driverFile"
 	$driverURL = Ninja-Property-Docs-Get "Printers" "$printerName" "driverUrl"
 	Write-Host "Driver URL: $driverURL"
-	$driverExeName = Ninja-Property-Docs-Get "Printers" "$printerName" "driverExeName"
-	if($driverExeName){Write-Host "Driver Switches: $driverExeName"}
 	$newDriverVersion = Ninja-Property-Docs-Get "Printers" "$printerName" "driverVersion"
 	Write-Host "Driver version: $newDriverVersion"
 	$newDriverName = Ninja-Property-Docs-Get "Printers" "$printerName" "driverName"
 	Write-Host "Driver name: $newDriverName"
 	$specifyInfFile = Ninja-Property-Docs-Get "Printers" "$printerName" "specifyInfFile"
 	Write-Host "INF file: $specifyInfFile"
-	$filterToNetworksWithGateway = Ninja-Property-Docs-Get "Printers" "$printerName" "filterToNetworksWithGateway"
-	if($filterToNetworksWithGateway){Write-Host "Networks: $filterToNetworksWithGateway"}
-	$filterToHostnames = Ninja-Property-Docs-Get "Printers" "$printerName" "filterToHostnames"
-	if($filterToHostnames){Write-Host "Hostnames: $filterToHostnames"}
 	$duplexMode = Ninja-Property-Docs-Get "Printers" "$printerName" "duplexMode"
-
 	Write-Host "Duplex setting: $duplexMode"
 	$color = Ninja-Property-Docs-Get "Printers" "$printerName" "color"
 	Write-Host "Color setting: $color"
 	$collate = Ninja-Property-Docs-Get "Printers" "$printerName" "collate"
 	Write-Host "Collate setting: $collate"
-
-	$tempFolder = "C:\Temp"
 	Write-Host "Temp folder: $tempFolder"
 	$downloadFile = "$tempFolder\$driverFile"
 	Write-Host "Temp driver download: $downloadFile"
@@ -76,68 +65,15 @@ foreach ($Printer in $Printers){
 	if($printerDriverExists){
 		$oldDriverName = $newDriverName
 	}
-
-	#First we assume that this host is not on the list for hostnames or networks
-	$thisPrinterShouldBeInstalled = $false
-
-	if($filterToNetworksWithGateway){
-		#We now iterate through all gateways as defined in the documentation
-		Write-Host "Network filtering enabled. Checking that this host has a matching gateway."
-		Foreach ($filterGateway in $filterToNetworksWithGateway){
-			 Foreach ($Gateway in $Gateways){
-					if($Gateway -eq $filterGateway){
-						 #We found a match between the documentation-defined subnets and a current subnet on this host! Installation should proceed
-						 Write-Host "Subnet match for $Gateway! Printer $printerName will be installed."
-						 $thisPrinterShouldBeInstalled = $true
-					}
-			 }
-		}
-	}
-
-	if($filterToHostnames){
-		#We now iterate through all hostnames as defined in the documentation
-
-		Foreach ($filterToHostname in $filterToHostnames){
-			 if($hostname -eq $filterToHostname){
-						 #We found a match between the documentation-defined hostname and this computer's hostname, the installation should proceed
-						 Write-Host "Hostname match! Printer $printerName will be installed."
-						 $thisPrinterShouldBeInstalled = $true
-					}
-			 }
-		}
-	
-	if(-not $filterToNetworksWithGateway -and -not $filterToHostnames){
-		#Since there is no filtering, this printer will be deployed to all hosts where the script runs
-		$thisPrinterShouldBeInstalled = $true
-	}
-
-	if(-not $thisPrinterShouldBeInstalled){
-		#Skipping installation, ensure printer is not installed on this host
-		Write-Host "Skipping printer $printerName - it should not be installed to this host's current subnet or hostname."
-		
-		#Checking if printer already exists. If so, remove printer
-		if($printerExists){
-				Write-Host "Printer $printerName exists, but is filtered out by hostname or subject. Removing..."
-				Remove-Printer -Name "$printerName" -ErrorAction SilentlyContinue
-		}
-		#Just some console text cleanliness
-		Write-Host "Printer $PrinterCount complete. `n`n"
-		
-		#Continue on to next printer in documentation
-		continue
-	}
 	
 	#We now know that the printer SHOULD exist on this host, so we want to check 
 	#if the driver name or version is different, in which case we will download,
 	#install and apply the new driver. First, we assume that we won't do any of that
 	$installNewDriver = $false
 	$upgradeDriver = $false
-
-
 	
 	#if print driver exists, collect the version number
-	if(-not $printerDriverExists)
-	{
+	if(-not $printerDriverExists) {
 		Write-Host "Driver $newDriverName not found. New driver will be installed."
 		$installNewDriver = $true
 	}else{
@@ -156,67 +92,47 @@ foreach ($Printer in $Printers){
 		if($newDriverVersion -ne $oldDriverVersion){
 			Write-Host "Old driver version doesn't match new driver version"
 			$upgradeDriver = $true
-		}
-		else{
-			
+		}else{
+			Write-Host "Old driver version matches new driver version"
+			$upgradeDriver = $false
 		}
 	}
-			
-
 	
 	#If driver doesn't exist or installed version is different, we need to download/extract the driver files
 	if($installNewDriver -or $upgradeDriver){
-		#Clean up existing print driver download
+		Write-Host "Clean up existing print driver download"
 		New-Item -ItemType Directory $tempFolder -ErrorAction SilentlyContinue
 		Remove-Item $downloadFile -Force -ErrorAction SilentlyContinue
 		Remove-Item $tempFolder\newDriver -Force -Recurse -ErrorAction SilentlyContinue
 		New-Item -ItemType Directory $tempFolder\newDriver -ErrorAction SilentlyContinue
 	
-		#Download driver file to $tempFolder
-		$webClient.DownloadFile($driverURL, $downloadFile)
-		#Invoke-WebRequest -Uri "$driverURL" -OutFile $downloadFile
+		Write-Host "Download driver file to $tempFolder"
+		(New-Object Net.WebClient).DownloadFile($driverURL, $downloadFile)
 				
 		Write-Host "Extracting $downloadFile to $tempFolder\newDriver"
-				
-		#Extract zip file containing drivers 
-		#& 'C:\Program Files\7-Zip\7z.exe' x $downloadFile "-o$tempFolder\newDriver" -y
-		tar -xf $downloadFile -C $tempFolder\newDriver
-		####################################################################
-		# Extract zip file containing drivers - this method is supposed		#
-		# to work but is commented out because I ran into "file in use"		#
-		# errors caused by our antivirus (Trend Micro)										 #
-		####################################################################
-		#Add-Type -Assembly "System.IO.Compression.Filesystem"
-		#[System.IO.Compression.ZipFile]::ExtractToDirectory('$downloadFile','$tempFolder\newDriver')
-		
-		if($driverExeName) {
-			Start-Process (Get-ChildItem -Recurse -Filter $driverExeName | Select-Object -First 1).fullname -ArgumentList "/VERYSILENT /DIR=$tempFolder\newDriver" 
+		switch ((Get-ChildItem $downloadFile).Extension) {
+			.zip {Write-Host "Extracting from zip"; tar -xf $downloadFile -C $tempFolder\newDriver}
+			.exe {Write-Host "Extracting from exe"; Start-Process $downloadFile -ArgumentList "/VERYSILENT /DIR=$tempFolder\newDriver" }
+			Default {Write-Warning "Error extracting drive, quiting"; exit}
 		}
 
 		Write-Host "Driver archive extracted"
 	}
 	
 	if($installNewDriver -or $upgradeDriver){
-				Write-Host "Driver $newDriverName $newDriverVersion is not installed. Installing..."
-		
-				#For clean installs
-				if(-not $upgradeDriver){
-				if(-not $specifyInfFile){
-						#Install all drivers in expanded folder
-						Write-Host "Installing all drivers in driver folder to system..."
-						Get-ChildItem "$tempFolder\newDriver" -Recurse -Filter "*.inf" | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
-				}
-		
-				#Install only specified INF file
-				if($specifyInfFile){
-						PNPUtil.exe /add-driver (Get-ChildItem "$tempFolder\newDriver" -Recurse -Filter "$specifyInfFile" | Select-Object -First 1).FullName /install
-				}
-				#Add requested driver name to local print server
-				$newDriverInfPath = Get-WindowsDriver -Online -All | Where-Object {$_.ClassName -eq "Printer" -and $_.Version -eq "$newDriverVersion"} | select -ExpandProperty OriginalFileName
-				Write-Host "Installing print driver: $newDriverName"
-				Write-Host "New driver inf path: $newDriverInfPath"
-				Add-PrinterDriver -Name "$newDriverName" -InfPath "$newDriverInfPath"
+		Write-Host "Driver $newDriverName $newDriverVersion is not installed. Installing..."
+		#For clean installs
+		if(-not $upgradeDriver){
+			#Install all drivers in expanded folder
+			Write-Host "Installing all drivers in driver folder to system..."
+			Get-ChildItem "$tempFolder\newDriver" -Recurse -Filter "*.inf" | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
 		}
+		
+		#Add requested driver name to local print server
+		$newDriverInfPath = Get-WindowsDriver -Online -All | Where-Object {$_.ClassName -eq "Printer" -and $_.Version -eq "$newDriverVersion"} | select -ExpandProperty OriginalFileName
+		Write-Host "Installing print driver: $newDriverName"
+		Write-Host "New driver inf path: $newDriverInfPath"
+		Add-PrinterDriver -Name "$newDriverName" -InfPath "$newDriverInfPath"
 	}
 
 	#Check if printer port already exists. If not, add it
@@ -234,8 +150,7 @@ foreach ($Printer in $Printers){
 		if (-not $printerExists){
 			Write-Host "Installing printer... this can take a few minutes"
 			Add-Printer -Name $printerName -PortName "$printerIP" -DriverName "$newDriverName"
-			}
-		else{
+		}else{
 			Write-Host "Printer already exists. Will update port and driver`n"
 			
 			Write-Host "Setting port to $printerIP"
@@ -245,17 +160,29 @@ foreach ($Printer in $Printers){
 			Set-Printer -Name $printerName -DriverName "$newDriverName"
 			}
 				
-		}
-	else{
+		}else{
 		Write-Warning "Error: Printer driver not installed, please check version number and URL to driver is correct"
 	}
 	
-	#Set printer defaults
+	Write-Host "Setting print configuration"
 	switch ($duplexMode) {
 		c9f09750-6b84-4a5b-b0b1-7ef9d6187bd1 {$duplexMode = "OneSided"}
 		316e6a9a-22da-4d36-bf2b-858b911c5c45 {$duplexMode = "TwoSidedLongEdge"}
 		79d617c5-d809-4ad6-b29d-2509afe8f297 {$duplexMode = "TwoSidedShortEdge"}
 	}
+
+	switch ($color) {
+		0 {[bool]$color=$false}
+		1 {[bool]$color=$true}
+		Default {Write-Warning "Error setting color config, exiting"; exit}
+	}
+	
+	switch ($collate) {
+		0 {[bool]$collate=$false}
+		1 {[bool]$collate=$true}
+		Default {Write-Warning "Error setting collate config, exiting"; exit}
+	}
+
 	Set-PrintConfiguration $printerName -DuplexingMode $duplexMode -Color $color -Collate $collate
 
 	#Just some console text cleanliness
@@ -267,61 +194,8 @@ Write-Host "Cleaning up temp files..."
 Remove-Item $downloadFile -Force -ErrorAction SilentlyContinue
 Remove-Item $tempFolder\newDriver -Force -Recurse -ErrorAction SilentlyContinue
 
-Write-Host "#######################"
-Write-Host "# Old printer cleanup #"
-Write-Host "#######################`n`n"
-
-#Find printers to delete from documentation and remove them
-Write-Host "Deleting unwanted printers..."
-$printersToDelete = Ninja-Property-Docs-Names "Printers To Delete"
-foreach ($Printer in $printersToDelete){
-	#split the results string to get just the name of the printer in documentation
-	$pos = $Printer.IndexOf("=")
-	$printerName = $Printer.Substring($pos+1)
-		
-	if(Get-Printer -Name "$printerName" -ErrorAction SilentlyContinue){
-		#remove all print jobs
-		Write-Host "Removing all printer jobs for printer: $printerName"
-		Get-PrintJob -PrinterName "$printerName" | Remove-PrintJob
-		
-		#delete printer
-		Write-Host "Deleting: $printerName..."
-		Remove-Printer -Name "$printerName" -ErrorAction SilentlyContinue
-	}
-}
-
-#Find print servers to delete from documentation and remove all associated printers
-Write-Host "Deleting printers from unwanted print servers..."
-
-#Build batch file to run on start-up
-$printServers = Ninja-Property-Docs-Names "Print Servers To Delete"
-if($printServers){
-	#Path to script has to be location accessible to all users
-	$deletePrintServersScriptPath = "C:\Users\Public\deletePrintServers.ps1"
-	$ScriptContent = $null
-	foreach ($printServer in $printServers){
-		#split the results string to get just the name of the print server in documentation
-		$pos = $printServer.IndexOf("=")
-		$printServerName = $printServer.Substring($pos+1)
-		$ScriptContent = $ScriptContent + "Remove-Printer -name \\$printServerName\*`n"
-	}
-
-	#Create (and erase existing) script file
-	New-Item -Path $deletePrintServersScriptPath -ItemType "File" -Force | Set-Content -Value $ScriptContent
-	
-	#Schedule script to run at login
-	schtasks /create /f /ru builtin\users /sc onlogon /tn "Remove printers from old print servers" /tr "powershell.exe -WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $deletePrintServersScriptPath"
-	$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-	Set-ScheduledTask -TaskName "Remove printers from old print servers" -Settings $settings
-}else{
-	#Delete scheduled task if no printers from any print server exist
-	if(Get-ScheduledTask -TaskName "Remove printers from old print servers" -ErrorAction SilentlyContinue){
-		Unregister-ScheduledTask -TaskName "Remove printers from old print servers" -Confirm:$False
-	}
-}
-
 #Clean up unused printer drivers
-$Drivers = Get-PrinterDriver
+$Drivers = Get-PrinterDriver | where {$_.name -ne "Microsoft enhanced Point and Print compatibility driver"}
 $Printers = Get-Printer
 
 ForEach($Driver in $Drivers)
@@ -350,3 +224,5 @@ catch {
 		Write-Host "Creating registry key $path..."
 }
 New-ItemProperty -Path $path -Name 'AutoSetup' -Value 0 -PropertyType Dword -Force
+
+Stop-Transcript
